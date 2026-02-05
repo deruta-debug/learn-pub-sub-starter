3eb0dad1d4068b08eb7bd25c8dac5cfde16f0e89
deruta <deruta.fuzzy377@gmail.com>
2026-02-04 19:40:32 -0500

    consume and persist logs to disk

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -26,17 +26,17 @@ func main() {
 		log.Fatalf("could not create channel: %v", err)
 	}
 
-	_, queue, err := pubsub.DeclareAndBind(
+	err = pubsub.SubscribeGob(
 		conn,
 		routing.ExchangePerilTopic,
 		routing.GameLogSlug,
 		routing.GameLogSlug+".*",
 		pubsub.SimpleQueueDurable,
+		handlerLogs(),
 	)
 	if err != nil {
 		log.Fatalf("could not subscribe to %s: %v", routing.GameLogSlug+".*", err)
 	}
-	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
 
 	gamelogic.PrintServerHelp()
 
